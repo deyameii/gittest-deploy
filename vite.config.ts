536ffec933ce5a2635ad228ea17a9b9cd5149f65
deyameii <behamoth91@icloud.com>
2023-01-13 23:34:51 +0200

    changes 2

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/gittest-deploy/'
-  plugins: [react()],
+	base: '/gittest-deploy/',
+	plugins: [react()]
 })
